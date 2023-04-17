b11ac71cddfef155c163115c32fcffd488199e06
Nauval Muhammad Firdaus <nauval.firdaus@accelbyte.net>
2023-04-17 08:24:26 +0700

    feat: add single platform auth tutorial module

---
+++ Source/AccelByteWars/TutorialModules/Module-4/UI/SinglePlatformAuthWidget_Starter.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+#include "TutorialModules/Module-4/UI/SinglePlatformAuthWidget_Starter.h"
+#include "TutorialModules/Module-2/UI/LoginWidget_Starter.h"
+#include "CommonButtonBase.h"
+
+void USinglePlatformAuthWidget_Starter::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	Btn_LoginWithSinglePlatformAuth->OnClicked().AddUObject(this, &ThisClass::OnLoginWithSinglePlatformAuthButtonClicked);
+
+	// TODO: Put the auto login with single auth platform implementation here.
+}
+
+void USinglePlatformAuthWidget_Starter::OnLoginWithSinglePlatformAuthButtonClicked()
+{
+	// TODO: Trigger login with single auth platform here.
+}
---
+++ Source/AccelByteWars/TutorialModules/Module-4/UI/SinglePlatformAuthWidget_Starter.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/UI/AccelByteWarsActivatableWidget.h"
+#include "SinglePlatformAuthWidget_Starter.generated.h"
+
+class UCommonButtonBase;
+
+UCLASS()
+class ACCELBYTEWARS_API USinglePlatformAuthWidget_Starter : public UAccelByteWarsActivatableWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	void NativeConstruct() override;
+
+private:
+	void OnLoginWithSinglePlatformAuthButtonClicked();
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, BlueprintProtected = true, AllowPrivateAccess = true))
+	UCommonButtonBase* Btn_LoginWithSinglePlatformAuth;
+};
