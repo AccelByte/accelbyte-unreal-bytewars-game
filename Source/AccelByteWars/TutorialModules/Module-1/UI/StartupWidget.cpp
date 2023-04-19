38f0ec78d820b97fb51fa2eda62695cc6aed8bb9
Nauval Muhammad Firdaus <nauval.firdaus@accelbyte.net>
2023-04-19 15:24:20 +0700

    chore: remove unnecessary files from module online

--- Source/AccelByteWars/TutorialModules/Module-1/UI/StartupWidget.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
-// This is licensed software from AccelByte Inc, for limitations
-// and restrictions contact your company contract manager.
-
-#include "TutorialModules/Module-1/UI/StartupWidget.h"
