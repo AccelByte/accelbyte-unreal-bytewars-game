74c71a224979acb7f603dea09f98729143387a4d
Nauval Muhammad Firdaus <nauval.firdaus@accelbyte.net>
2023-04-20 08:15:44 +0700

    chore: rename single platform auth folder from module-4 to module-7

--- Source/AccelByteWars/TutorialModules/Module-7/UI/SinglePlatformAuthWidget_Starter.cpp
+++ Source/AccelByteWars/TutorialModules/Module-7/UI/SinglePlatformAuthWidget_Starter.cpp
@@ -2,7 +2,7 @@
 // This is licensed software from AccelByte Inc, for limitations
 // and restrictions contact your company contract manager.
 
-#include "TutorialModules/Module-4/UI/SinglePlatformAuthWidget_Starter.h"
+#include "TutorialModules/Module-7/UI/SinglePlatformAuthWidget_Starter.h"
 #include "TutorialModules/Module-2/AuthEssentialsSubsystem_Starter.h"
 #include "TutorialModules/Module-2/UI/LoginWidget_Starter.h"
 #include "CommonButtonBase.h"
