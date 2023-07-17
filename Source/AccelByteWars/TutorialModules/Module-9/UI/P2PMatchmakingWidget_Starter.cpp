ffbad86443bfbfb0a10452112c627ff0333629b4
Nauval Muhammad Firdaus <nauval.firdaus@accelbyte.net>
2023-07-17 08:27:47 +0700

    chore: refactor tutorial modules widget containers assignment

--- Source/AccelByteWars/TutorialModules/Module-9/UI/P2PMatchmakingWidget_Starter.cpp
+++ Source/AccelByteWars/TutorialModules/Module-9/UI/P2PMatchmakingWidget_Starter.cpp
@@ -3,10 +3,8 @@
 // and restrictions contact your company contract manager.
 
 #include "TutorialModules/Module-9/UI/P2PMatchmakingWidget_Starter.h"
-#include "Core/System/AccelByteWarsGameInstance.h"
 #include "Core/UI/AccelByteWarsBaseUI.h"
 #include "CommonButtonBase.h"
-
 #include "TutorialModules/Module-9/P2PMatchmakingSubsystem_Starter.h"
 #include "TutorialModules/Module-3/UI/QuickPlayWidget.h"
 #include "TutorialModules/Module-3/UI/QuickPlayWidget_Starter.h"
@@ -27,33 +25,30 @@ void UP2PMatchmakingWidget_Starter::NativeDestruct()
 
 void UP2PMatchmakingWidget_Starter::OnStartP2PMatchmakingButtonClicked()
 {
-	UAccelByteWarsGameInstance* GameInstance = Cast<UAccelByteWarsGameInstance>(GetGameInstance());
-	ensure(GameInstance);
-
-	UAccelByteWarsBaseUI* BaseUIWidget = GameInstance->GetBaseUIWidget();
-	ensure(BaseUIWidget);
-
-	APlayerController* PC = GetWorld()->GetFirstPlayerController();
-	ensure(PC);
+	UCommonActivatableWidget* ParentWidget = UAccelByteWarsBaseUI::GetActiveWidgetOfStack(EBaseUIStackType::Menu, this);
+	if (!ParentWidget)
+	{
+		return;
+	}
 
 	UTutorialModuleDataAsset* MatchmakingEssentialsModule = UTutorialModuleUtility::GetTutorialModuleDataAsset(FPrimaryAssetId("TutorialModule:MATCHMAKINGESSENTIALS"), this);
 	ensure(MatchmakingEssentialsModule);
 
-	UP2PMatchmakingSubsystem_Starter* P2PMatchmakingSubsystem = GameInstance->GetSubsystem<UP2PMatchmakingSubsystem_Starter>();
+	UP2PMatchmakingSubsystem_Starter* P2PMatchmakingSubsystem = GetGameInstance()->GetSubsystem<UP2PMatchmakingSubsystem_Starter>();
 	ensure(P2PMatchmakingSubsystem);
 
 	// Use Matchmaking's default files if starter mode is not active.
 	if (!MatchmakingEssentialsModule->IsStarterModeActive())
 	{
-		UQuickPlayWidget* ParentWidget = Cast<UQuickPlayWidget>(BaseUIWidget->Stacks[EBaseUIStackType::Menu]->GetActiveWidget());
-		ensure(ParentWidget);
+		UQuickPlayWidget* QuickPlayWidget = Cast<UQuickPlayWidget>(ParentWidget);
+		ensure(QuickPlayWidget);
 
 		// When the cancel matchmaking clicked, handle the matchmaking cancelation through the P2P matchmaking subsystem.
-		ParentWidget->OnRequestCancelMatchmaking.AddUObject(P2PMatchmakingSubsystem, &UP2PMatchmakingSubsystem_Starter::CancelMatchmaking);
+		QuickPlayWidget->OnRequestCancelMatchmaking.AddUObject(P2PMatchmakingSubsystem, &UP2PMatchmakingSubsystem_Starter::CancelMatchmaking);
 
 		// Request matchmaking using P2P server. Match pool format: unreal-{gamemode}-p2p.
-		const FString MatchPool = FString::Printf(TEXT("unreal-%s-p2p"), *ParentWidget->GetMatchGameMode());
-		P2PMatchmakingSubsystem->StartMatchmaking(GetOwningPlayer(), MatchPool, FOnMatchmakingStateChangedDelegate::CreateUObject(ParentWidget, &UQuickPlayWidget::OnMatchmaking));
+		const FString MatchPool = FString::Printf(TEXT("unreal-%s-p2p"), *QuickPlayWidget->GetMatchGameMode());
+		P2PMatchmakingSubsystem->StartMatchmaking(GetOwningPlayer(), MatchPool, FOnMatchmakingStateChangedDelegate::CreateUObject(QuickPlayWidget, &UQuickPlayWidget::OnMatchmaking));
 	}
 	// Use Matchmaking's starter files if starter mode is not active.
 	else
