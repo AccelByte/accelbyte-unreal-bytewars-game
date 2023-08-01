4694ab0f6dcd507a3f031ea76ff9fb35af5fc6c3
Nauval Muhammad Firdaus <nauval.firdaus@accelbyte.net>
2023-08-01 14:08:31 +0700

    feat(leaderboard): add weekly periodic leaderboard for each game modes

---
+++ Source/AccelByteWars/TutorialModules/Module-13/UI/LeaderboardWeeklyWidget_Starter.h
@@ -0,0 +1,41 @@
+// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/UI/AccelByteWarsActivatableWidget.h"
+#include "TutorialModules/Module-13/PeriodicBoardSubsystem_Starter.h"
+#include "LeaderboardWeeklyWidget_Starter.generated.h"
+
+class UAccelByteWarsWidgetList;
+class ULeaderboardWidgetEntry;
+
+UCLASS()
+class ACCELBYTEWARS_API ULeaderboardWeeklyWidget_Starter : public UAccelByteWarsActivatableWidget
+{
+	GENERATED_BODY()
+
+protected:
+#pragma region Module.13 Function Declarations
+	// TODO: Add your protected Module.13 function declarations here.
+#pragma endregion
+
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeOnActivated() override;
+
+	UPeriodicBoardSubsystem_Starter* PeriodicLeaderboardSubsystem;
+	FString LeaderboardCode;
+	FString CycleId;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 ResultLimit = 10;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional, BlueprintProtected = true, AllowPrivateAccess = true))
+	UAccelByteWarsWidgetList* WidgetList;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional, BlueprintProtected = true, AllowPrivateAccess = true))
+	ULeaderboardWidgetEntry* PlayerRankPanel;
+};
