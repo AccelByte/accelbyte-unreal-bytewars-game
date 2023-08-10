f5757b6f09ae1922389852d5474359dc0e4362c4
Nauval Muhammad Firdaus <nauval.firdaus@accelbyte.net>
2023-08-10 14:26:31 +0700

    feat(multi-environment): add param launch to switch between environment

--- Source/AccelByteWars/TutorialModules/Module-1/TutorialModuleOnlineUtility.cpp
+++ Source/AccelByteWars/TutorialModules/Module-1/TutorialModuleOnlineUtility.cpp
@@ -6,8 +6,14 @@
 #include "Core/Utilities/AccelByteWarsBlueprintFunctionLibrary.h"
 #include "OnlineSubsystemUtils.h"
 #include "Core/AccelByteRegistry.h"
+#include "AccelByteUe4SdkModule.h"
 
 DEFINE_LOG_CATEGORY(LogAccelByteWarsTutorialModuleOnlineUtility);
+
+UTutorialModuleOnlineUtility::UTutorialModuleOnlineUtility()
+{
+    CheckForEnvironmentConfigOverride();
+}
 
 bool UTutorialModuleOnlineUtility::IsAccelByteSDKInitialized(const UObject* Target)
 {
@@ -46,4 +52,61 @@ bool UTutorialModuleOnlineUtility::IsAccelByteSDKInitialized(const UObject* Targ
     }
 
     return IsOSSEnabled && !IsSDKCredsEmpty;
+}
+
+void UTutorialModuleOnlineUtility::CheckForEnvironmentConfigOverride()
+{
+    FString CmdArgs = FCommandLine::Get();
+    if (!CmdArgs.Contains(TEXT("-TARGET_ENV="), ESearchCase::IgnoreCase))
+    {
+        return;
+    }
+
+    FString EnvironmentStr;
+    FParse::Value(FCommandLine::Get(), TEXT("-TARGET_ENV="), EnvironmentStr);
+    if (!EnvironmentStr.IsEmpty())
+    {
+        ESettingsEnvironment ABEnvironment = ConvertStringEnvToAccelByteEnv(EnvironmentStr);
+        IAccelByteUe4SdkModuleInterface::Get().SetEnvironment(ABEnvironment);
+    }
+    else
+    {
+        UE_LOG_TUTORIAL_MODULE_ONLINE_UTILITY(Warning, TEXT("Cannot change target environment. Desired target environment cannot be found from command line."));
+    }
+}
+
+ESettingsEnvironment UTutorialModuleOnlineUtility::ConvertStringEnvToAccelByteEnv(const FString& EnvironmentStr)
+{
+    if (EnvironmentStr.Equals(FString("Development"), ESearchCase::IgnoreCase))
+    {
+        return ESettingsEnvironment::Development;
+    }
+    else if (EnvironmentStr.Equals(FString("Certification"), ESearchCase::IgnoreCase))
+    {
+        return ESettingsEnvironment::Certification;
+    }
+    else if (EnvironmentStr.Equals(FString("Production"), ESearchCase::IgnoreCase))
+    {
+        return ESettingsEnvironment::Production;
+    }
+    else
+    {
+        return ESettingsEnvironment::Default;
+    }
+}
+
+ESettingsEnvironment UTutorialModuleOnlineUtility::ConvertOSSEnvToAccelByteEnv(const EOnlineEnvironment::Type& Environment)
+{
+    switch (Environment)
+    {
+        case EOnlineEnvironment::Type::Development:
+            return ESettingsEnvironment::Development;
+        case EOnlineEnvironment::Type::Certification:
+            return ESettingsEnvironment::Certification;
+        case EOnlineEnvironment::Type::Production:
+            return ESettingsEnvironment::Production;
+        case EOnlineEnvironment::Type::Unknown:
+        default:
+            return ESettingsEnvironment::Default;
+    }
 }
