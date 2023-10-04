5b95e95257efd38a370fc5091c9bb16ca4b78126
RozyZx <fachrurrozy@accelbyte.net>
2023-10-04 14:32:17 +0700

    fix(PlayingWIthFriends): Invite Friends button shown in non Match Session lobby

---
+++ Source/AccelByteWars/TutorialModules/PlayingWithFriends/UI/InviteFriendsWidget.cpp
@@ -0,0 +1,64 @@
+﻿// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+
+#include "InviteFriendsWidget.h"
+
+#include "CommonButtonBase.h"
+#include "Core/System/AccelByteWarsGameInstance.h"
+#include "Core/UI/AccelByteWarsBaseUI.h"
+#include "TutorialModules/PlayingWithFriends/PlayingWithFriendsSubsystem.h"
+
+void UInviteFriendsWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	bool bShowButton = false;
+
+	if (const UPlayingWithFriendsSubsystem* Subsystem = GetGameInstance()->GetSubsystem<UPlayingWithFriendsSubsystem>())
+	{
+		bShowButton =
+			Subsystem->IsInMatchSessionGameSession() &&
+			UTutorialModuleUtility::IsTutorialModuleActive(FPrimaryAssetId{ "TutorialModule:FRIENDSESSENTIALS" }, this);
+	}
+
+	SetVisibility(bShowButton ? ESlateVisibility::Visible : ESlateVisibility::Collapsed);
+
+	Btn_Friends->OnClicked().AddUObject(this, &ThisClass::OpenFriendsMenu);
+}
+
+void UInviteFriendsWidget::NativeDestruct()
+{
+	Super::NativeDestruct();
+
+	Btn_Friends->OnClicked().RemoveAll(this);
+}
+
+void UInviteFriendsWidget::OpenFriendsMenu() const
+{
+	UAccelByteWarsGameInstance* GameInstance = Cast<UAccelByteWarsGameInstance>(GetGameInstance());
+	if (!GameInstance)
+	{
+		return;
+	}
+
+	UAccelByteWarsBaseUI* BaseUIWidget = GameInstance->GetBaseUIWidget();
+	if (!BaseUIWidget)
+	{
+		return;
+	}
+
+	const UTutorialModuleDataAsset* DataAsset = UTutorialModuleUtility::GetTutorialModuleDataAsset(
+		FPrimaryAssetId{ "TutorialModule:FRIENDSESSENTIALS" },
+		this,
+		true);
+	if (!DataAsset)
+	{
+		return;
+	}
+
+	BaseUIWidget->PushWidgetToStack(
+		EBaseUIStackType::Menu,
+		DataAsset->IsStarterModeActive() ? FriendsWidgetClass_Starter : FriendsWidgetClass);
+}
---
+++ Source/AccelByteWars/TutorialModules/PlayingWithFriends/UI/InviteFriendsWidget.h
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/UI/AccelByteWarsActivatableWidget.h"
+#include "InviteFriendsWidget.generated.h"
+
+class UCommonButtonBase;
+
+UCLASS(Abstract)
+class ACCELBYTEWARS_API UInviteFriendsWidget : public UAccelByteWarsActivatableWidget
+{
+	GENERATED_BODY()
+
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+private:
+	void OpenFriendsMenu() const;
+
+private:
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional, BlueprintProtected = true, AllowPrivateAccess = true))
+	UCommonButtonBase* Btn_Friends;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAccelByteWarsActivatableWidget> FriendsWidgetClass;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAccelByteWarsActivatableWidget> FriendsWidgetClass_Starter;
+};
