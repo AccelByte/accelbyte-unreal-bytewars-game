354b7e857ed8d97e6fb4af1a82a40adf2763be37
Fachrurrozy Muhammad <fachrurrozy@accelbyte.net>
2024-01-04 02:45:28 +0000

    Merged in feature/DEVED-742-selection-widget-component (pull request #118)
    feat(core): add sequential selection widget component
    
    * feat: add sequential selection widget component
    
    * chore: change parameters name to be clearer and add in-line docs
    
    
    Approved-by: Nicky Jovanus

---
+++ Source/AccelByteWars/Core/UI/Components/AccelByteWarsSequentialSelectionWidget.cpp
@@ -0,0 +1,107 @@
+﻿// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+
+#include "AccelByteWarsSequentialSelectionWidget.h"
+
+#include "CommonButtonBase.h"
+#include "CommonInputSubsystem.h"
+#include "Components/TextBlock.h"
+#include "Core/Utilities/AccelByteWarsUtility.h"
+
+void UAccelByteWarsSequentialSelectionWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	Tb_DisplayName->SetText(DisplayName);
+	SetSelection(Selections, CurrentIndex);
+}
+
+void UAccelByteWarsSequentialSelectionWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	Btn_CycleRight->OnClicked().AddUObject(this, &ThisClass::CycleSelection, ECycleDirection::RIGHT);
+	Btn_CycleLeft->OnClicked().AddUObject(this, &ThisClass::CycleSelection, ECycleDirection::LEFT);
+}
+
+void UAccelByteWarsSequentialSelectionWidget::NativeDestruct()
+{
+	Btn_CycleRight->OnClicked().RemoveAll(this);
+	Btn_CycleLeft->OnClicked().RemoveAll(this);
+
+	Super::NativeDestruct();
+}
+
+FNavigationReply UAccelByteWarsSequentialSelectionWidget::NativeOnNavigation(
+	const FGeometry& MyGeometry,
+	const FNavigationEvent& InNavigationEvent,
+	const FNavigationReply& InDefaultReply)
+{
+	if (InNavigationEvent.GetNavigationType() == EUINavigation::Left)
+	{
+		CycleSelection(ECycleDirection::LEFT);
+		return FNavigationReply::Explicit(nullptr);
+	}
+	else if (InNavigationEvent.GetNavigationType() == EUINavigation::Right)
+	{
+		CycleSelection(ECycleDirection::RIGHT);
+		return FNavigationReply::Explicit(nullptr);
+	}
+
+	return Super::NativeOnNavigation(MyGeometry, InNavigationEvent, InDefaultReply);
+}
+
+void UAccelByteWarsSequentialSelectionWidget::SetSelection(TArray<FText>& InSelections, int32 DefaultIndex)
+{
+	Selections = InSelections;
+	CurrentIndex = DefaultIndex < InSelections.Num() && DefaultIndex >= 0 ? DefaultIndex : INDEX_NONE;
+
+	DrawSelection();
+}
+
+void UAccelByteWarsSequentialSelectionWidget::ClearSelection()
+{
+	Selections.Empty();
+	CurrentIndex = INDEX_NONE;
+
+	DrawSelection();
+}
+
+void UAccelByteWarsSequentialSelectionWidget::SetDisplayName(const FText InText) const
+{
+	Tb_DisplayName->SetText(InText);
+}
+
+void UAccelByteWarsSequentialSelectionWidget::SetSelectedIndex(const int32 Index)
+{
+	CurrentIndex = Index;
+	DrawSelection();
+}
+
+void UAccelByteWarsSequentialSelectionWidget::CycleSelection(const ECycleDirection Direction)
+{
+	int32 Modifier = 0;
+	switch (Direction)
+	{
+	case ECycleDirection::RIGHT:
+		Modifier = 1;
+		break;
+	case ECycleDirection::LEFT:
+		Modifier = -1;
+		break;
+	}
+	CurrentIndex = AccelByteWarsUtility::PositiveModulo(CurrentIndex + Modifier, Selections.Num());
+
+	DrawSelection();
+}
+
+void UAccelByteWarsSequentialSelectionWidget::DrawSelection()
+{
+	Tb_Selected->SetText(CurrentIndex > INDEX_NONE ? Selections[CurrentIndex] : FText());
+
+	const bool bShouldEnableButton = Selections.Num() > 1;
+	Btn_CycleRight->SetIsEnabled(bShouldEnableButton);
+	Btn_CycleLeft->SetIsEnabled(bShouldEnableButton);
+}
---
+++ Source/AccelByteWars/Core/UI/Components/AccelByteWarsSequentialSelectionWidget.h
@@ -0,0 +1,68 @@
+﻿// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "Core/UI/AccelByteWarsWidgetInterface.h"
+#include "AccelByteWarsSequentialSelectionWidget.generated.h"
+
+class UTextBlock;
+class UCommonButtonBase;
+
+UCLASS(Abstract)
+class ACCELBYTEWARS_API UAccelByteWarsSequentialSelectionWidget : public UCommonUserWidget, public IAccelByteWarsWidgetInterface
+{
+	GENERATED_BODY()
+
+	virtual void NativePreConstruct() override;
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	virtual FNavigationReply NativeOnNavigation(
+		const FGeometry& MyGeometry,
+		const FNavigationEvent& InNavigationEvent,
+		const FNavigationReply& InDefaultReply) override;
+
+public:
+	void SetSelection(TArray<FText>& InSelections, int32 DefaultIndex = 0);
+	void ClearSelection();
+
+	void SetDisplayName(const FText InText) const;
+
+	void SetSelectedIndex(const int32 Index);
+	int32 GetSelectedIndex() const { return CurrentIndex; }
+	FText GetSelected() const { return CurrentIndex > INDEX_NONE ? Selections[CurrentIndex] : FText(); }
+
+private:
+	enum class ECycleDirection : uint8
+	{
+		RIGHT = 0,
+		LEFT = 1
+	};
+
+	void CycleSelection(const ECycleDirection Direction);
+	void DrawSelection();
+
+	UPROPERTY(EditInstanceOnly)
+	TArray<FText> Selections;
+
+	UPROPERTY(EditInstanceOnly)
+	int32 CurrentIndex = 0;
+
+	UPROPERTY(EditInstanceOnly)
+	FText DisplayName;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, BlueprintProtected = true, AllowPrivateAccess = true))
+	UTextBlock* Tb_Selected;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, BlueprintProtected = true, AllowPrivateAccess = true))
+	UTextBlock* Tb_DisplayName;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, BlueprintProtected = true, AllowPrivateAccess = true))
+	UCommonButtonBase* Btn_CycleRight;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, BlueprintProtected = true, AllowPrivateAccess = true))
+	UCommonButtonBase* Btn_CycleLeft;
+};
